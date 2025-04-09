e549a4b9edbde600c8aa02aec61eea664a8c6e0c
rafiahmedrizvee <rizvirafi7@gmail.com>
2025-04-09 19:08:07 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <div>
+    <div className='max-w-[1440px] mx-auto'>
       <RouterProvider router={router}></RouterProvider>
     </div>
     </>
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,14 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
 
 const Header = () => {
-    return (
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/home">Home</Link>
-          <Link to="/about">About</Link>
+  const menuItems = (
+    <React.Fragment>
+      <li>
+        <Link to= '/' >Home</Link>
+      </li>
+      <li>
+        <NavLink to= '/appointment' >Appointment</NavLink>
+      </li>
+      <li>
+        <Link to= '/about' >About</Link>
+      </li>
+      <li>
+        <NavLink to= '/contact' >Contact</NavLink>
+      </li>
+      
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      <div className="navbar shadow-sm px-5">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {menuItems}
+            </ul>
+          </div>
+          <Link to="/" className="w-28">
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+         {menuItems}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
