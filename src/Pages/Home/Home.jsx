cbc106a3282fb52ed96673d95e0d35f8d8a47fd6
rafiahmedrizvee <rizvirafi7@gmail.com>
2025-04-10 23:37:49 +0600

    basic done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from './Banner/Banner';
+import Services from './Services/Services';
+import Testimonial from './Testimonial/Testimonial';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home page</h1>
+            <Banner></Banner>
+            <Services></Services>
+            <Testimonial></Testimonial>
         </div>
     );
 };
